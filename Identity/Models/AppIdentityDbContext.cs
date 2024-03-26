d11f349c2eec170c670cbf769ce696f5f2da0b60
teboho <tebohomakhalemele@outlook.com>
2024-03-26 15:12:43 +0200

    migrations.. add-migration  [name] update-database ...

--- Identity/Models/AppIdentityDbContext.cs
+++ Identity/Models/AppIdentityDbContext.cs
@@ -5,10 +5,12 @@ namespace Identity.Models
 {
     /// <summary>
     /// Inheriting abilities to manipulate the AppUser entity in the database using EntityFrameworkCore
+    /// One benefit is that we dont need to manually declare the user entity as a property of the context clas...
     /// </summary>
     public class AppIdentityDbContext : IdentityDbContext<AppUser>
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
-        { }
+        { 
+        }
     }
 }
