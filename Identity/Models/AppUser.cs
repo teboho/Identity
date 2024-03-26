67becd0c200513fab9e258f36f1a8afc32ff2b74
teboho <tebohomakhalemele@outlook.com>
2024-03-26 14:32:56 +0200

    describing the AppUser entity model

--- Identity/Models/AppUser.cs
+++ Identity/Models/AppUser.cs
@@ -2,6 +2,12 @@
 
 namespace Identity.Models
 {
+    /// <summary>
+    /// We inherit some properties from the IdentityUser class:
+    ///     - id, username, claims, email, phone number, password hash, roles, security stamp,...etc
+    /// <br />
+    /// We can define additional properties to our users if there is a need
+    /// </summary>
     public class AppUser : IdentityUser
     {
     }
