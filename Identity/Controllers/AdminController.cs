39f0ebfc97160801b023ec6965014c40f5794501
teboho <tebohomakhalemele@outlook.com>
2024-03-26 16:09:01 +0200

    basic cruds (some)

---
+++ Identity/Controllers/AdminController.cs
@@ -0,0 +1,127 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        /// <summary>
+        /// A repository to make changes to the User data
+        /// </summary>
+        private UserManager<AppUser> userManager;
+        private IPasswordHasher<AppUser> passwordHasher;
+        /// <summary>
+        /// we get the UserManager through dependency injection
+        /// </summary>
+        /// <param name="userMan">the injected user manager object</param>
+        public AdminController(UserManager<AppUser> userMan, IPasswordHasher<AppUser> pHasher)
+        {
+            userManager = userMan;
+            this.passwordHasher = pHasher;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(userManager.Users);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            } 
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                {
+                    user.Email = email;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email Cannot Be Empty");
+                }
+
+                if (!string.IsNullOrEmpty(password))
+                {
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email Cannot Be Empty");
+                }
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else Errors(result);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email Cannot Be Empty");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
