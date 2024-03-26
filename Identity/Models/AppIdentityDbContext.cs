43ad7759b64a74d03c49ef3b4c2d3122b171d468
teboho <tebohomakhalemele@outlook.com>
2024-03-26 14:33:25 +0200

    Database  context to be used by Entity Framework Core

---
+++ Identity/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Models
+{
+    /// <summary>
+    /// Inheriting abilities to manipulate the AppUser entity in the database using EntityFrameworkCore
+    /// </summary>
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        { }
+    }
+}
